e4d2c29189b68b36f9636ee08114bb1fa1af5176
Ezeki Zibzibadze <ez@jok.io>
2014-02-19 11:39:34 -0800

    added v1 folders

---
+++ Jok.Play/Jok.Play/Scripts/V1/Play/jok.js
@@ -0,0 +1,292 @@
+﻿var jok = jok || {};
+var game = game || {};
+
+
+(function () {
+
+    var userAgent = navigator.userAgent.toLowerCase();
+    var isMobileDevice = (userAgent.indexOf('iphone') > -1 || userAgent.indexOf('iphone') > -1 || userAgent.indexOf('iphone') > -1);
+
+    jok.config = jok.config || {};
+
+    var defaultConfig = {
+        gameid: 0,
+        channel: '',
+        language: 'en_US',
+        apiUrl: 'http://api.jok.ge/',
+        pusherUrl: 'http://pusher.jok.io:80',
+        smilesLocation: 'http://jok.io/content/images/skype/',
+        pluginUrl: 'http://play.jok.io',
+        exitUrl: 'http://jok.io',
+        playUrl: window.location,
+        authorizationUrl: 'http://jok.io/joinus?returnUrl=' + window.location,
+        isMobileDevice: isMobileDevice,
+        checkFriendText: '[cmd:CheckFriendsStatus]',
+        isVIPMember: false,
+        buzzText: '[cmd:Buzz]',
+        audios: '/audios'
+    }
+
+    for (prop in defaultConfig) {
+        jok.config[prop] = jok.config[prop] || defaultConfig[prop];
+    }
+
+})();
+
+var clickEvent = jok.config.isMobileDevice ? 'touchstart' : 'click';
+
+jok.players = {};
+
+jok.init = function () {
+
+    var toggleFullScreen = function () {
+
+        var isInFullScreen =
+            (document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method  
+            (document.mozFullScreen || document.webkitIsFullScreen);
+
+
+        var el = document.documentElement
+          , requestFullScreen = el.requestFullScreen
+                             || el.webkitRequestFullScreen
+                             || el.mozRequestFullScreen
+
+          , exitFullScreen = document.cancelFullScreen
+                          || document.webkitCancelFullScreen
+                          || document.mozCancelFullScreen;
+
+
+        if (!requestFullScreen || !exitFullScreen) return;
+
+        if (!isInFullScreen)
+            requestFullScreen.call(el);
+        else
+            exitFullScreen.call(document);
+    }
+
+
+    if ($.cookie('sidebar_open2') == '1') {
+        $('#RightDoc').css('display', 'block');
+        $('#RightDoc').css('right', '0px');
+        $('#Root').css('right', '230px');
+        $('body').css('margin-right', '230px');
+    }
+
+    $(document).on(clickEvent, '#RightDocOpener', function () {
+        jok.openDockRight();
+    });
+
+    $(document).on(clickEvent, '#RightDocClose', function () {
+        jok.closeDockRight();
+    });
+
+    $(document).on(clickEvent, '.player .rate .step1 button', function () {
+        var userid = $(this).parent().parent().parent().parent().attr('data-userid');
+        var rate = $(this).attr('data-value');
+        var position = $(this).parent().parent().parent().attr('data-position');
+
+        if (!userid || !rate) return;
+
+        jok.rateUser(position, userid, rate);
+    });
+
+    $(document).on(clickEvent, '.player .rate .step2', function () {
+        $(this).parent().hide('fast');
+    });
+
+
+
+    $(document).on(clickEvent, '#ExitButton', function () {
+        window.location.assign(jok.config.exitUrl);
+    });
+
+    $(document).bind('touchmove', '#Root', function (e) {
+        //e.preventDefault();
+    });
+
+
+
+    $(document).on(clickEvent, '.sendFriendRequestButton', function () {
+
+        $(this).hide();
+
+        var opponentUserID = $(this).attr('data-userid');
+        if (!opponentUserID) return;
+
+        jok.api.sendFriendRequest(opponentUserID);
+
+        jok.chat.send(jok.texts.A026);
+    });
+
+    // addins
+    $(document).on(clickEvent, '#FullScreen', toggleFullScreen);
+    $(document).on(clickEvent, '#Addins > .item.full_screen', toggleFullScreen);
+
+    $(document).on(clickEvent, '#Addins > .item > .bg', function () {
+        $('#Addins > .item > #SmilesBox').toggle();
+    });
+
+
+    $(document).on(clickEvent, '.player .buzz', function () {
+        jok.buzz();
+
+        var _this = $(this);
+
+        _this.hide();
+
+        setTimeout(function () {
+            _this.show();
+        }, 30 * 1000);
+
+        jok.chat.sendInternal(jok.config.buzzText);
+        jok.chat.send('BUZZ!');
+    });
+
+    $(document).on(clickEvent, '#Notification .item.share_friends input', function () {
+        $(this).select();
+    });
+
+    $('#Audios').append('<audio class="buzz" preload="auto"><source src="' + jok.config.pluginUrl + '/Content/Audios/buzz.mp3" /></audio>');
+
+
+    if ($.cookie('isMusicPlaying') == '1') {
+        $('.music_speakers').show();
+    }
+
+
+    jok.chat.init();
+}
+
+jok.getCookieDomain = function () {
+    var firstIndex = window.location.host.indexOf('.');
+    var lastIndex = window.location.host.lastIndexOf('.');
+    var host = window.location.host;
+
+    return (firstIndex != lastIndex) ? host.substring(firstIndex) : (lastIndex > -1 ? '.' + host : host);
+}
+
+jok.buzz = function () {
+    $('#Game').effect('shake', { times: 3 }, 200);
+    jok.audio.play('buzz');
+}
+
+jok.setPlayer = function (position, userid) {
+
+    if (userid == null || userid == 0) {
+        $('#Player' + position).hide();
+        return;
+    }
+
+    jok.api.getUser(userid, function (isSuccess, user) {
+        if (!isSuccess) return;
+
+        jok.players[user.id] = user;
+
+        var player = $('#Player' + position); // 1 or 2
+        if (player.length == 0) return;
+
+        player.find('.nick').html(user.nick);
+        player.find('.avatar').attr('src', user.avatarUrl);
+        player.show();
+
+        player.attr('data-userid', user.id);
+
+        if (user.id == jok.currentUserID) {
+            jok.config.isVIP = user.isVIP;
+            $('#SmilesBox .vip_smiles_container').attr('class', user.isVIP ? 'vip_smiles_container' : 'vip_smiles_container disabled');
+            if (user.isVIP) {
+                $('.player .buzz').show();
+            }
+            else {
+                $('#Banner').show();
+            }
+        } else {
+            jok.chat.check(user);
+
+            if (user.isVIP) {
+                player.find('.rate .step1 .fan').show();
+            } else {
+                player.find('.rate .step1 .fan').hide();
+            }
+        }
+
+        var playerDetails = player.children('.details');
+        if (playerDetails.length == 0) return;
+
+        playerDetails.find('.info > .avatar').attr('src', user.avatarUrl);
+        playerDetails.find('.info > .nick').html(user.nick);
+        playerDetails.find('.stats .points .value').html(user.points);
+        playerDetails.find('.stats .fans .value').html(user.fans);
+        playerDetails.find('.stats .friends .value').html(user.friendsCount);
+
+        if (user.isVIP)
+            playerDetails.find('.stats .vip').show();
+        else
+            playerDetails.find('.stats .vip').hide();
+
+        playerDetails.find('.actions > .sendFriendRequestButton').hide();
+        playerDetails.find('.actions > .friend_status').html('');
+
+        switch (user.friendStatus) {
+            case 1:
+                playerDetails.find('.actions > .friend_status').html('You are Friends');
+
+                break;
+
+            case 2:
+                playerDetails.find('.actions > .sendFriendRequestButton').attr('data-userid', user.id);
+                playerDetails.find('.actions > .sendFriendRequestButton').show();
+
+                break;
+
+            case 3:
+                playerDetails.find('.actions > .friend_status').html('Waiting answer friend request');
+
+                break;
+        }
+    });
+}
+
+jok.showUserRate = function (position) {
+    $('#Player' + position + ' .rate .step1').show();
+    $('#Player' + position + ' .rate .step2').hide();
+    $('#Player' + position + ' .rate').attr('data-position', position);
+    $('#Player' + position + ' .rate').show('fast');
+}
+
+jok.rateUser = function (position, userid, rate) {
+
+    jok.api.rateUser(userid, jok.config.gameid, rate);
+
+    $('#Player' + position + ' .rate .step1').hide();
+    $('#Player' + position + ' .rate .step2').show();
+
+    setTimeout(function () {
+        $('#Player' + position + ' .rate').hide('fast');
+    }, 4000);
+}
+
+
+jok.openDockRight = function () {
+    $('#Root').animate({ right: 230 }, 'fast');
+    $('body').animate({ marginRight: 230 }, 'fast');
+
+    $('#RightDoc').css('display', 'block');
+    $('#RightDoc').animate({ right: 0 }, 'fast');
+
+    $.cookie('sidebar_open2', '1', { expires: 300, path: '/' });
+}
+
+jok.closeDockRight = function () {
+    $('#Root').animate({ right: 0 }, 'normal');
+    $('body').animate({ marginRight: 0 }, 'normal');
+
+    $('#RightDoc').animate({ right: -230 }, 'normal', function () {
+        $('#RightDoc').css('display', 'none');
+    });
+
+    $.cookie('sidebar_open2', '0', { expires: 300, path: '/' });
+    $.removeCookie('sidebar_open2');
+}
+
+jok.currentUserID = undefined;
---
+++ Jok.Play/Jok.Play/Scripts/V1/jok.ui-builder.js
@@ -0,0 +1,88 @@
+﻿
+
+(function (jok) {
+
+    var texts = jok.texts;
+
+    var musicPlayerPlugin = $('<div class="jokfm_plugin"></div>');
+    musicPlayerPlugin.append('<div class="item"><img class="next_button" src="' + jok.config.pluginUrl + '/images/FM/plugin/next.png" /></div>');
+    musicPlayerPlugin.append('<div class="item"><img class="play_button" src="' + jok.config.pluginUrl + '/images/FM/plugin/play.png" /><div class="stop_button"></div></div>');
+    musicPlayerPlugin.append('<div class="item"><img class="previous_button" src="' + jok.config.pluginUrl + '/images/FM/plugin/previous.png" /></div>');
+    musicPlayerPlugin.append('<div class="active_channel title">' + texts.A006 + '</div>');
+
+    var musicPlayer = $('<div class="music_player"></div>');
+    musicPlayer.append('<div class="jokfm_plugin_volume"><div class="logo"></div><div class="volume_opener"><div class="volume-selector"></div></div></div>');
+    musicPlayer.append(musicPlayerPlugin);
+
+    var playerDetails = $('<div class="details"></div>');
+    playerDetails.append('<div class="info"><img class="avatar" src="" alt="" /><div class="nick"></div></div>');
+    playerDetails.append('<div class="stats"> <table cellpadding="0px" cellspacing="0px" class="stats-menu"> <tr class="vip"> <td class="description">' + texts.A001 + ': </td> <td style="color: Red;" class="value">VIP </td> </tr> <tr class="points"> <td class="description">' + texts.A002 + ': </td> <td style="color: orange;" class="value">0 </td> </tr> <tr class="fans"> <td class="description">' + texts.A003 + ': </td> <td style="color: #2AAA00;" class="value">0 </td> </tr> <tr class="friends"> <td class="description">' + texts.A004 + ': </td> <td style="color: #AC0909;" class="value">0 </td> </tr> </table> </div> <div class="actions"> <div class="friend_status"></div> <button class="button green sendFriendRequestButton">' + texts.A005 + '</button> </div> </div>');
+
+    var player = $('<div class="player"></div>');
+    player.append('<img src="' + jok.config.pluginUrl + '/images/speakers' + (jok.config.isVIPMember ? '2' : '') + '.png" class="music_speakers" />');
+    player.append('<img class="avatar" src="" alt="" />');
+    player.append('<div class="nick"></div>');
+    player.append('<div class="progresbar"></div>');
+    player.append('<div class="score"></div>');
+    player.append('<div class="added_points">+<span>0</span> ' + texts.A007 + '</div>');
+    player.append('<div class="wins"></div>');
+    player.append('<div class="chat_bubble"><div class="msg"></div></div>');
+    player.append('<div class="button red buzz">BUZZ!!!</div>');
+    player.append('<div class="ready"><img src="' + jok.config.pluginUrl + '/Images/ready.png" /></div>');
+
+    var player1 = player.clone(false);
+    player1.attr('id', 'Player1');
+    player1.append(musicPlayer);
+    player1.find('.buzz').remove();
+
+    var player2 = player.clone(false);
+    player2.attr('id', 'Player2');
+    player2.append(playerDetails);
+    player2.find('.chat_bubble').attr('class', 'chat_bubble invert');
+
+    var notifications = $('<div id="Notification"></div>');
+    notifications.append('<div class="item connecting">' + texts.A008 + '<br /><br /><img src="' + jok.config.pluginUrl + '/images/loading_black.gif" alt="connecting" /></div>');
+    notifications.append('<div class="item require_authorization">' + texts.A009 + '</div>');
+    notifications.append('<div class="item waiting_opponent"><br />' + texts.A010 + '<br /><br /></div>');
+    notifications.append('<div class="item share_friends">' + texts.A011 + '<br /><br /><input type="text" value="' + jok.config.playUrl + '" /><div class="info_msg"></div></div>');
+    notifications.append('<div class="item quit">' + texts.A012 + '<br /><span>MESSAGE GOES HERE</span><br /><button class="button brown new_game">' + texts.A013 + '</button></div>');
+    notifications.append('<div class="item table_leave_winner"><span style="font-size: 13px">' + texts.A014 + '</span><br /><br /><button class="button brown new_game">' + texts.A015 + '</button></div>');
+    notifications.append('<div class="item table_finish_winner">' + texts.A016 + ' <span>WINNER NAME GOES HERE</span><br /><br /><button class="button brown play_again">' + texts.A017 + '</button></div>');
+    notifications.append('<div class="item tournament_game_limit">' + texts.A028 + '<br /><br /><button class="button brown new_game">' + texts.A029 + '</button></div>');
+
+    var rightDocOpener = $('<div id="RightDocOpener"><span>' + texts.A018 + '</span></div>');
+
+    var rightDoc = $('<div id="RightDoc"></div>');
+    rightDoc.append('<div id="RightDocClose"></div>');
+    rightDoc.append('<div id="SmilesBox"> <div class="separator"> <span>' + texts.A019 + '</span> </div> <div class="smiles_container"> </div> <div class="more"> ' + texts.A020 + ' </div> <div class="vip_smiles"> <div class="separator"> <span>' + texts.A021 + '</span> </div> <div class="vip_smiles_container"> </div> <div class="less"> ' + texts.A022 + ' </div> </div> </div>');
+    rightDoc.append('<div id="ChatBox"> <div class="container"> </div> <div class="separator"> <span>' + texts.A023 + '</span> </div> <input class="user_text" type="text" placeholder="' + texts.A024 + '" /> </div>');
+
+    var audios = $('<div id="Audios"></div>');
+
+    var info = $('<div id="Info"></div>')
+    info.append(notifications);
+    info.append(player1);
+    info.append(player2);
+
+
+    var background1 = $('<div id="BackgroundMaterial"></div>');
+    var background2 = $('<div id="Background"></div>');
+    var loader = $('<div id="Loader"><span class="percentage">0</span>%</div>');
+    var root = $('<div id="Root"></div>');
+    root.append(info);
+    root.append(rightDocOpener);
+    root.append(rightDoc);
+    root.append(audios);
+    var exit = $('<div id="ExitButton">' + texts.A025 + '</div>');
+    var addins = $('<div id="Addins"><div class="item full_screen"></div></div>');
+
+    var jokLayout = $('<div id="jok"></div>');
+    jokLayout.append(background1);
+    jokLayout.append(background2);
+    jokLayout.append(loader);
+    jokLayout.append(root);
+    jokLayout.append(exit);
+    jokLayout.append(addins);
+
+    $('body').prepend(jokLayout);
+})(jok);
